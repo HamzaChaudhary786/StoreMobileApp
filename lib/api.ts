d70e3355727c180098dcdbb917927b0615aa0907
HamzaChaudhary786 <hmzsattar99@gmail.com>
2026-05-01 11:26:30 +0500

    url changed

--- lib/api.ts
+++ lib/api.ts
@@ -4,7 +4,7 @@ import { Platform } from 'react-native';
 // For Android Emulator, localhost is 10.0.2.2
 // For iOS Simulator, localhost is 127.0.0.1
 // For physical devices, use your computer's local IP address
-const API_URL = 'http://192.168.1.6:5000/api';
+const API_URL = 'https://store-manage-backend.vercel.app/api';
 
 export const api = axios.create({
   baseURL: API_URL,
